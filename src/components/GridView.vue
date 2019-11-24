dead5c3fdb159468e181981ad609f7b19f494647
Charlotte Hintzen <s77294@htw-dresden.de>
2019-11-24 17:05:48 +0100

    images margin

--- src/components/GridView.vue
+++ src/components/GridView.vue
@@ -6,6 +6,10 @@
       class="image"
     >
       <b-img :src="image.src"></b-img>
+
+      <div v-if="imgArray[index] % 2 === 0">
+        <div class="break"></div>
+      </div>
     </div>
   </div>
 </template>
@@ -16,7 +20,10 @@ export default {
   data() {
     return {
       imgArray: [
-        { name: "Apfel", src: "https://picsum.photos/250/250/?image=58" },
+        {
+          name: "Apfel",
+          src: "https://picsum.photos/250/250/?image=58"
+        },
         { name: "Apfel", src: "https://picsum.photos/250/250/?image=58" },
         { name: "Apfel", src: "https://picsum.photos/250/250/?image=58" },
         { name: "Apfel", src: "https://picsum.photos/250/250/?image=58" }
@@ -29,12 +36,20 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 #immagewrapper {
-  background-color: powderblue;
+  background-color: rgb(12, 143, 160);
   display: flex;
+  flex-direction: row;
   flex-wrap: wrap;
 }
 .image {
   max-width: 500px;
+  margin: 5px;
   float: left;
+  align-content: space-between;
+}
+.break {
+  background-color: rgb(12, 143, 160);
+  src: "https://picsum.photos/250/250/?image=58";
+  src: "https://picsum.photos/250/250/?image=54";
 }
 </style>
